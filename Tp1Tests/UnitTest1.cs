a53c6f2ee84b6b1846aa515833af1296864606e4
cedryk567 <150829578+cedryk567@users.noreply.github.com>
2025-10-09 11:49:43 -0400

    Premier Testunitaire pour ajouter des students

--- Tp1Tests/UnitTest1.cs
+++ Tp1Tests/UnitTest1.cs
@@ -1,10 +1,30 @@
 ﻿namespace Tp1Tests;
+using SchoolManager;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void AcceptMemberType_DeStudent_WithConsoleTyping()
     {
+        string name = "Alice";
+        string address = "101 avenue du Nord";
+        string phone = "514";
+        string grade = "40";
+        int choiceAddStudent = 4;
+        int choiceAction = 1;
 
+        string simulatedInput = $"{name}\n{address}\n{phone}\n{grade}";
+        var originalIn = Console.In;
+        Console.SetIn(new StringReader(simulatedInput));
+        
+        bool result = StrategiesMembers[choiceAddStudent].MakeChoice(choiceAction);
+
+        var student = Program.Students[0];
+        Assert.Equal(true, result);
+        Assert.Equal("Alice", student.Name);
+        Assert.Equal("101 avenue du Nord", student.Address);
+        Assert.Equal(514, student.Phone);
+        Assert.Equal(85, student.Grade);
+        Console.SetIn(originalIn);
     }
 }
