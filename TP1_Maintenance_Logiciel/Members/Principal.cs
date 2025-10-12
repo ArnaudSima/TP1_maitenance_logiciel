21837a1a49ac8b4cbe7bbce90750da37ae0e202c
cedryk567 <150829578+cedryk567@users.noreply.github.com>
2025-10-12 19:31:28 -0400

    Programmation défensive pour le principal

--- TP1_Maintenance_Logiciel/Members/Principal.cs
+++ TP1_Maintenance_Logiciel/Members/Principal.cs
@@ -15,12 +15,33 @@ namespace SchoolManager
 
         public Principal(int income = MembersSalary.PrincipalSalary)
         {
+            if(income <= 0)
+            {
+                Console.WriteLine("Warning: The income cannot be negative or equal zero");
+                income = 0;
+            }
             Income = income;
             Balance = 0;
         }
 
         public Principal(string name, string address, int phoneNum)
         {
+            if (string.IsNullOrEmpty(name) || string.IsNullOrWhiteSpace(name))
+            {
+                Console.WriteLine("Warning: The name cannot be empty");
+                name = "default name";
+            }
+            if (string.IsNullOrEmpty(address) || string.IsNullOrWhiteSpace(address))
+            {
+                Console.WriteLine("Warning: The address cannot be empty");
+                address= "default address";
+            }
+            if (phoneNum <= 0 || phoneNum.ToString().Length < 8)
+            {
+                Console.WriteLine("Warning: The phone number cannot be empty, equal to zero or longer then 8 characters");
+                phoneNum = 0;
+            }
+   
             Name = name;
             Address = address;
             Phone = phoneNum;
@@ -50,6 +71,8 @@ namespace SchoolManager
 
         public override Action Add => () =>
         {
+            string nameTest, addressTest, phoneTest;
+            bool entrerValide = true;
             UndoEntry entry = new UndoEntry();
             entry.Undo = () =>
             {
@@ -58,9 +81,82 @@ namespace SchoolManager
             entry.Description = $"Removing the principal : {ToString()}";
             UndoManager.Push(entry);
             Console.WriteLine("Please enter the Principals information.");
-            Program.Principal.Name = ConsoleHelper.AskQuestion("Enter name: ");
-            Program.Principal.Address = ConsoleHelper.AskQuestion("Enter Address: ");
-            Program.Principal.Phone = Int32.Parse(ConsoleHelper.AskQuestion("Enter Phone: "));
+            
+            //tester le nom
+            nameTest = ConsoleHelper.AskQuestion("Enter name: ");
+            if (string.IsNullOrEmpty(nameTest) || string.IsNullOrWhiteSpace(nameTest))
+            {
+                Console.WriteLine("Warning: The name cannot be empty");
+                entrerValide = false;
+            }
+            while (!entrerValide)
+            {
+                nameTest = ConsoleHelper.AskQuestion("Enter name: ");
+                if (string.IsNullOrEmpty(nameTest) || string.IsNullOrWhiteSpace(nameTest))
+                {
+                    Console.WriteLine("Warning: The name cannot be empty");
+                }
+                else
+                {
+                    entrerValide = true;
+                }
+            }
+
+            //tester l'address
+            addressTest = ConsoleHelper.AskQuestion("Enter Address: ");
+            if (string.IsNullOrEmpty(addressTest) || string.IsNullOrWhiteSpace(addressTest))
+            {
+                Console.WriteLine("Warning: The address cannot be empty");
+                entrerValide = false;
+            }
+            while (!entrerValide)
+            {
+                addressTest = ConsoleHelper.AskQuestion("Enter Address: ");
+                if (string.IsNullOrEmpty(addressTest) || string.IsNullOrWhiteSpace(addressTest))
+                {
+                    Console.WriteLine("Warning: The address cannot be empty");
+                }
+                else
+                {
+                    entrerValide = true;
+                }
+            }
+
+            //tester le numero de telephone
+            phoneTest = ConsoleHelper.AskQuestion("Enter Phone: ");
+            int phoneInput;
+            if (!int.TryParse(phoneTest, out phoneInput) )
+            {
+                Console.WriteLine("Warning: The phone number must have only number. ");
+                
+            }else if (phoneInput == 0 || string.IsNullOrWhiteSpace(phoneTest) || string.IsNullOrEmpty(phoneTest))
+            {
+                Console.WriteLine("Warning: The phone number cannot be equal to zero or empty ");
+                entrerValide = false;
+            }
+            while (!entrerValide)
+            {
+                phoneTest = ConsoleHelper.AskQuestion("Enter Phone: ");
+                if (!int.TryParse(phoneTest, out phoneInput))
+                {
+                    Console.WriteLine("Warning: The phone number must have only number. ");
+
+                }
+                else if (phoneInput == 0 || string.IsNullOrWhiteSpace(phoneTest) || string.IsNullOrEmpty(phoneTest))
+                {
+                    Console.WriteLine("Warning: The phone number cannot be equal to zero or empty ");
+                    entrerValide = false;
+                }
+                else
+                {
+                    entrerValide = true;
+                }
+            }
+
+            //création du principal
+            Program.Principal.Name = nameTest;
+            Program.Principal.Address = addressTest;
+            Program.Principal.Phone = int.Parse(phoneTest);
             Program.Flag = true;
         };
 
